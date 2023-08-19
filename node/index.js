22809b2d193bcc0cea19f251bcdb76afffe135f8
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-19 15:00:56 +0330

    add: logs export on file

--- node/index.js
+++ node/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
-const port = process.env.HTTP_PORT;
+const port = process.env.HTTP_PORT??8080;
+
+const fs = require('fs');
+const util = require('util');
+const logFile = fs.createWriteStream('log.txt', { flags: 'a' });
+const logStdout = process.stdout;
+
+
+console.error = console.log;
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log(`${new Date()}[${req.method}] path: ${req.url}`);
+    res.send('Hello World!');
 });
 
+app.post('/log', (req, res) => {
+    console.log(`${new Date()}[${req.method}] path: ${req.url}`);
+    res.send('Logged!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`example app listening on port ${port}`);
+
+  console.log = function () {
+    logFile.write(util.format.apply(null, arguments) + '\n');
+    logStdout.write(util.format.apply(null, arguments) + '\n');
+  }
 });

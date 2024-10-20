68751ef8c03addd3b126849f0a37a2d3a0d8055b
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-19 22:51:31 -0400

    init: handler.c

---
+++ src/handler.c
@@ -0,0 +1,9 @@
+// get the input type from user
+// if it is by password, send your public key
+// client sends a random string and password using public key encryption
+// use that key to communicate.
+
+// if it is by key
+// get user key and look for matching in authorized_leys
+// if matched, server sends a key using public key encryption
+// client uses that to communicate

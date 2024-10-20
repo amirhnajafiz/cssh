f3050b09dc05a1cbb52a98a7f26618cbf0683f0d
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-20 00:25:20 -0400

    feat: socket server

--- src/server.c
+++ src/server.c
@@ -1,17 +1,18 @@
-#include <winsock2.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <handler.h>
+#include <winsock2.h>
+#include "handler.h"
 #include "panic.h"
 
 // start is used to create a new socket server that accepts
 // ssh requests and manages users commands.
 int start(unsigned short port)
 {
-    struct sockaddr_in clinet; // clinet information
     struct sockaddr_in server; // server address information
 
-    int ss; // a socket for accepting connections
-    int ns; // a socket connected to client
+    int ss;      // a socket for accepting connections
+    int ns;      // a socket connected to client
+    int namelen; // length of client name
 
     // get a socket for accepting connections
     if ((ss = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -33,5 +34,26 @@ int start(unsigned short port)
     if (listen(ss, 1) != 0)
     {
         panic("failed to listen for input connections");
+    }
+
+    // server while loop
+    while (true)
+    {
+        // accept a connection
+        struct sockaddr_in client; // clinet information
+        namelen = sizeof(client);
+        if ((ns = accept(ss, (struct sockaddr *)&client, &namelen)) == -1)
+        {
+            panic("failed to accept a client socket");
+        }
+        else
+        {
+            int pid = fork(); // create a new sub-process for each user
+            if (pid == 0)
+            {
+                // in the child process, run the client handler function
+                client_handler(ns, namelen, &client);
+            }
+        }
     }
 }
